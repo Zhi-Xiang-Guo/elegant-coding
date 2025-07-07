c7d972026200b925f7a35bb73d53fa8a5ea08037
Xiang <140319155+Zhi-Xiang-Guo@users.noreply.github.com>
2025-07-07 10:45:20 +0800

    init

---
+++ src/main/java/elegantCondition.java
@@ -0,0 +1,22 @@
+
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author guozhixiang <guozhixiang@kuaishou.com>
+ * Created on 2025-07-07
+ */
+public class elegantCondition {
+    public static void main(String[] args) {
+
+    }
+    private void xxxMethod(String key){
+        if(key!=null&&!"".equals(key)){
+            // do something
+        }
+//        if(StringUtils.isNotEmpty(key)){
+//            // do something
+//        }
+    }
+}
